060f9c4be4f5c346158d5ea69698669edaf67cbf
PINTU KUMAR SAH <126938670+pintukumar5628@users.noreply.github.com>
2024-11-29 22:43:54 +0530

    SQL Query 3

---
+++ SQLQuery3.sql
@@ -0,0 +1,16 @@
+
+
+select * into #iteam1
+from [dbo].[Employees]
+
+select * from #iteam1
+
+select * from dbo.Employees
+
+select * into  ##iteam2
+from [dbo].[EmployeeRecords]
+
+select * from ##iteam2
+select * from dbo.EmployeeRecords
+
+
