a14cea71924ca62af3baeab339780ecadbc0100b
PINTU KUMAR SAH <126938670+pintukumar5628@users.noreply.github.com>
2024-12-06 22:52:02 +0530

    SQL Query 11

---
+++ SQLQuery11.sql
@@ -0,0 +1,22 @@
+
+
+select * from dbo.Employees
+
+select * into #3 from dbo.Employees
+select * from #3
+
+delete from #3
+where LastName = '', or department = '0'
+
+select * from Employees
+
+select * into #4 from dbo.Employees
+select * from #4
+delete from #4 
+
+select * from #3
+
+truncate * from #4
+
+drop * from #4
+
