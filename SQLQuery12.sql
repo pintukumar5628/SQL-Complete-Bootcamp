53be19d0a36dfb1efa844f4c213658610b7dc283
PINTU KUMAR SAH <126938670+pintukumar5628@users.noreply.github.com>
2024-12-07 22:59:21 +0530

    SQL Query 12

---
+++ SQLQuery12.sql
@@ -0,0 +1,25 @@
+
+ --Hi we are learning MS SQL Server 
+
+select * from dbo.Employees
+
+/* Hi
+we
+are 
+learning
+MS
+SQL
+server
+*/
+
+select * from dbo.EmployeeRecords
+
+select * from dbo.Employees
+
+select top 2 * from dbo.Employees
+
+select top 5 * from dbo.EmployeeRecords
+
+select top 2 employeeid, salary from Employees
+
+select top 5 firstName, LastName from EmployeeRecords
